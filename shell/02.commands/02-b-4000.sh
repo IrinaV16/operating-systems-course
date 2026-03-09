07473c3dd4ef9dd281cd907045ae663a3fd169e2
Irina Vlaykova <irinavlaykova@gmail.com>
2026-03-09 23:17:00 +0200

    Update 02-b-4000.sh

--- shell/02.commands/02-b-4000.sh
+++ shell/02.commands/02-b-4000.sh
@@ -1,1 +1,3 @@
 # Копирайте най-малкия файл от тези, намиращи се в /etc, в home директорията си.
+
+find /etc -maxdepth 1 -type f -printf "%s %f\n" | sort -n | head -n 1 | awk '{print $2}' | xargs -I {} cp /etc/{} ~
