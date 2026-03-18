bfc94a24a8c8ac3b02bcebb98753c18f87a2e88a
Irina Vlaykova <irinavlaykova@gmail.com>
2026-03-18 15:21:57 +0200

    Update 05-b-3100.sh

--- shell/05.scripts/05-b-3100.sh
+++ shell/05.scripts/05-b-3100.sh
@@ -1,2 +1,14 @@
 # Да се напише shell скрипт, който приканва потребителя да въведе низ - потребителско име на потребител от системата
 # - след което извежда на стандартния изход колко активни сесии има потребителят в момента.
+
+read -p 'Enter name' name
+
+if ! grep -q $name /etc/passwd ; then
+    echo "No person with ${name}"
+    exit 1
+
+fi
+
+activeSession=$(who | grep $name | wc -l)
+echo "The user has ${activeSessions) active sessions"
+  
